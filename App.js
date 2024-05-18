c4b6d2b9504b4dfb67c0e243aad8bc59c8f3bcc7
758140 <maike.758140@graduacao.fsa.br>
2024-05-17 21:52:51 -0300

    Create App.js
    Criação do arquivo app.js que verifica se um número é par ou ímpar

---
+++ App.js
@@ -0,0 +1,11 @@
+// Definindo uma lista de números
+let numeros = [2,4,6,8,10]
+// Percorrendo uma lista com loop for
+for(let i = 0; i < numeros.lengt; i++){
+   // Verificando se o número é par ou ímpar
+  if(numeros{i} % === 0){
+    console.log(numero[i] + " é um número par):
+  }else{
+    console.log(numero[i] + " é um numero impar);
+    }
+}
